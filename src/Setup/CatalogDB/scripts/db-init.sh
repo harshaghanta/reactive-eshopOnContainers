05cfc7393481512a2ebbb2f53ed21d5d1e185e31
SriHarsha Ghanta <harshaghanta@gmail.com>
2023-04-06 23:29:07 +0530

    Added sample bootstrap code to create catalogdb container with db , tables & data

---
+++ src/Setup/CatalogDB/scripts/db-init.sh
@@ -0,0 +1,9 @@
+# Wait for sql server to startup
+sleep 90s
+
+# Run the setup script to create the DB and the schema in the DB
+# Note: make sure that your password matches what is in the Dockerfile
+
+echo 'entered db-init.sh'
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssword1 -d master -i CatalogDB.sql
---
+++ src/Setup/CatalogDB/scripts/entrypoint.sh
@@ -0,0 +1,2 @@
+# Run Microsoft SQl Server and initialization script (at the same time)
+/usr/dbscripts/db-init.sh & /opt/mssql/bin/sqlservr
